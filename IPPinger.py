14fe03bc83e928b36c681823888fcaa5e04627dd
RhymePlays <63958689+RhymePlays@users.noreply.github.com>
2023-04-03 01:02:13 +0600

    Improved IPPinger

--- IPPinger.py
+++ IPPinger.py
@@ -5,8 +5,15 @@ IPQueueList = []
 
 # Parameters
 reqTimeout = 0.5
-numberOfThreads = 5
+numberOfThreads = 10
+logFileDirectory = "PingedIP.log"
 
+i1List = range(10, 11)
+i2List = range(10, 11)
+i3List = range(10, 11)
+i4List = range(0, 256)
+
+# Functions
 def testIP(ip, timeout):
     try:
         req = requests.get(ip, timeout=timeout)
@@ -18,7 +25,7 @@ def scannerThread(queueListIndex):
     for ip in IPQueueList[queueListIndex]:
         returnValue = testIP(ip, reqTimeout)
 
-        with open("file.log", "a") as f: f.write("\n"+json.dumps(returnValue)+",")        
+        with open(logFileDirectory, "a") as f: f.write("\n"+json.dumps(returnValue)+",")        
         print(returnValue)
 
 def start():
@@ -28,11 +35,11 @@ def start():
 
     # Populate Queues
     toWhichQueue=0
-    for i1 in range(10, 11):
-        for i2 in range(10, 11):
-            for i3 in range(10, 11):
-                for i4 in range(0, 256):
-                    IPQueueList[toWhichQueue].append(f"http://{i1}.{i2}.{i3}.{i4}")
+    for i1 in i1List:
+        for i2 in i2List:
+            for i3 in i3List:
+                for i4 in i4List:
+                    IPQueueList[toWhichQueue].append(f"http://{i1}.{i2}.{i3}.{i4}:8080")
 
                     if toWhichQueue >= numberOfThreads - 1: toWhichQueue = 0
                     else: toWhichQueue = toWhichQueue + 1
